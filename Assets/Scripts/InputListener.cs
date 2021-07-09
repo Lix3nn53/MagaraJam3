e7e82692a2ad6fb68f8bbef510c3bb61cc380267
Lix3nn53 <lixakyildiz@gmail.com>
2021-07-10 02:11:54 +0300

    Create isometric demo scene following unity tutorial

---
+++ Assets/Scripts/InputListener.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputListener : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+
+    
+    }
+}
