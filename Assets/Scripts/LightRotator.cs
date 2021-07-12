0990bf7893ef47a7c51ac3828cb0f4627e70a136
Lix3nn53 <lixakyildiz@gmail.com>
2021-07-12 04:38:17 +0300

    Sleep time

---
+++ Assets/Scripts/LightRotator.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class LightRotator : MonoBehaviour
+{
+    public float min;
+    public float max;
+    public float speed = 10f;
+    private Light2D light;
+    private bool increase = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = GetComponent<Light2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (increase) {
+            transform.Rotate(new Vector3(0f, 0f, speed * Time.deltaTime));
+            if (transform.rotation.z >= max) {
+                increase = false;
+            }
+        } else {
+            transform.Rotate(new Vector3(0f, 0f, -speed * Time.deltaTime));
+            if (transform.rotation.z <= min) {
+                increase = true;
+            }
+        }
+    }
+}
